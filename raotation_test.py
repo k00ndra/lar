d3420fd1807031e3d5e6fa52b4592e1aa3b58022
koondra <ondra.kodejs@gmail.com>
2023-03-01 13:02:39 +0100

    pushed test file

---
+++ raotation_test.py
@@ -0,0 +1,18 @@
+from robolab_turlebot import Turtlebot,Rate,get_time
+
+turle = Turtlebot()
+refresh_rate = 10 #Hz
+time_limit = 10 #s
+angular_speed = 0.8#rad/s
+rate = Rate(refresh_rate)
+
+
+start_time = get_time()
+while get_time() - start_time <time_limit:
+    turle.cmd_velocity(angular = angular_speed)
+    rate.sleep()
+
+    
+
+
+
