00797e234c4c2f566e72d40ed8cae2803d397188
koondra <ondra.kodejs@gmail.com>
2023-03-01 14:02:53 +0100

    markers update

--- show_markers_test.py
+++ show_markers_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import cv2
-
 from robolab_turtlebot import Turtlebot, detector
+import numpy as np
 
 WINDOW = 'markers'
 
@@ -10,25 +10,25 @@ WINDOW = 'markers'
 def main():
 
     turtle = Turtlebot(rgb=True,pc = True)
-    turtle.play_sound(sound_id = 3)
+    turtle.play_sound(sound_id = 0)
     cv2.namedWindow(WINDOW)
 
     while not turtle.is_shutting_down():
         # get point cloud
         image = turtle.get_rgb_image()
-
+        #point_cloud=turtle.get_point_cloud()
+        
         # wait for image to be ready
         if image is None:
             continue
+        
+        lower_yellow = np.array([100,100,0])
+        upper_yellow = np.array([255,255,50])
 
-        # detect markers in the image
-        markers = detector.detect_markers(image)
-
-        # draw markers in the image
-        detector.draw_markers(image, markers)
-
+        yellow_mask = cv2.inRange(image,lower_yellow,upper_yellow)
+        highlighted_markers = cv2.bitwise_and(image,image,yellow_mask)
         # show image
-        cv2.imshow(WINDOW, image)
+        cv2.imshow(WINDOW, highlighted_markers)
         cv2.waitKey(1)
 
 
