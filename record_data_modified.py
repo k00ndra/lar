314110b39710d09ab3a572a2b57f40fd494d05b2
koondra <ondra.kodejs@gmail.com>
2023-03-08 13:38:25 +0100

    take photo on bumber push

--- record_data_modified.py
+++ record_data_modified.py
@@ -6,26 +6,35 @@ from robolab_turtlebot import Turtlebot, sleep
 
 from scipy.io import savemat
 
+def bumper_cb(msg):
+    # get K, images, and point cloud
+    print("Bumper registered -> saving image")
+    data = dict()
+    data['K_rgb'] = turtle.get_rgb_K()
+    data['K_depth'] = turtle.get_depth_K()
+    data['image_rgb'] = cv2.cvtColor(turtle.get_rgb_image(),cv2.COLOR_BGR2RGB)
+    data['image_depth'] = turtle.get_depth_image()
+    data['point_cloud'] = turtle.get_point_cloud()
+
+    # save data to .mat file
+    filename = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+    savemat(filename+"_rgb.mat", data)
+
+    #save hsv:
+    data['image_rgb'] = cv2.cvtColor(data['image_rgb'],cv2.COLOR_BGR2HSV)
+    savemat(filename+"_hsv.mat", data)
+
+    print('Data saved in {}'.format(filename))
+
+
+
 # initialize turlebot
 turtle = Turtlebot(rgb=True, depth=True, pc=True)
 
 # sleep 2 set to receive images
-sleep(2)
+while True:
+    sleep(2)
 
-# get K, images, and point cloud
-data = dict()
-data['K_rgb'] = turtle.get_rgb_K()
-data['K_depth'] = turtle.get_depth_K()
-data['image_rgb'] = cv2.cvtColor(turtle.get_rgb_image(),cv2.COLOR_BGR2RGB)
-data['image_depth'] = turtle.get_depth_image()
-data['point_cloud'] = turtle.get_point_cloud()
 
-# save data to .mat file
-filename = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
-savemat(filename+"_rgb.mat", data)
 
-#save hsv:
-data['image_rgb'] = cv2.cvtColor(data['image_rgb'],cv2.COLOR_BGR2HSV)
-savemat(filename+"_hsv.mat", data)
 
-print('Data saved in {}'.format(filename))
